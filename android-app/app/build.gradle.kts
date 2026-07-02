4e4cb94d1ba0195ddc3bf7586180e10498f5d547
endri Susanto <endrisusantosekarang@gmail.com>
2026-07-02 19:14:41 +0700

    release v0.1.13

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.firefiles.bridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 112
-        versionName = "0.1.12"
+        versionCode = 113
+        versionName = "0.1.13"
     }
 
     compileOptions {
